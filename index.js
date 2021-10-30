a21e37cf563124eae974e472bd42dc65acaba960
ghost <imransardar122@gmail.com>
2021-10-30 12:11:00 +0600

    single route api

--- index.js
+++ index.js
@@ -4,9 +4,12 @@ const app = express()
 require('dotenv').config()
 const cors = require('cors')
 const port = process.env.PORT || 5000
+
 //configure midleware cors
 app.use(cors());
 app.use(express.json());
+//single data loading by objectId
+const ObjectId = require('mongodb').ObjectId;
 //connect to db
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.uvy8c.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -24,6 +27,23 @@ async function run() {
             const services = await cursor.toArray();
             res.send(services)
         });
+
+
+        //API for single data load
+        app.get('/services/:id', async (req, res) => {
+            const id = req.params.id;
+
+            const query = { _id: ObjectId(id) };
+
+            const service = await servicesCollection.findOne(query);
+            res.json(service);
+        })
+
+
+
+
+
+
         console.log('db connected');
     } finally {
 
